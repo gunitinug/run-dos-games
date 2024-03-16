8627446b7363df259049ddb725cf6fbecc720109
gunitinug <gunitinug@gmail.com>
2024-03-16 12:16:54 +1100

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,17 @@
+# Run a game from ~/my-dos-games folder
+
+# show list of games to run
+echo Here are list of games to run:
+ls *.sh | grep -v menu.sh | sed = | sed 'N;s/\n/\. /'
+echo -n "Enter the number: "
+
+read choice
+
+# get upper
+upper="$(ls *.sh | grep -v menu.sh | wc -l)"
+
+if [[ "$choice" =~ ^[1-$upper]$ ]]; then
+	bash "$(ls *.sh | grep -v menu.sh | awk -v c="$choice" 'NR==c')"
+else
+	echo Wrong choice!
+fi
